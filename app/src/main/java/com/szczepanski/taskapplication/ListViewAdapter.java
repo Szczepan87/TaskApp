cfc0b1f0f05d72152dcac417e4042675a168a372
Łukasz Szczepański <l.szczepan@wp.pl>
2018-11-13 14:57:27 +0100

    prygotowanie pod wyświetlanie widoków w ListView

--- app/src/main/java/com/szczepanski/taskapplication/ListViewAdapter.java
+++ app/src/main/java/com/szczepanski/taskapplication/ListViewAdapter.java
@@ -1,10 +1,42 @@
 package com.szczepanski.taskapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class ListViewAdapter extends ArrayAdapter {
 
     private Activity context;
+    private String[] IDs;
+    private String[] names;
+    private String[] statuses;
+
+    public ListViewAdapter(Activity context, String[] IDs, String[] names, String[] statuses) {
+        super(context, R.layout.listview_row, IDs);
+
+        this.context = context;
+        this.IDs = IDs;
+        this.names = names;
+        this.statuses = statuses;
+    }
+
+    public View addView(int position, View view, ViewGroup parent){
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.listview_row,null,true);
+
+        TextView ID = rowView.findViewById(R.id.task_Id_textView);
+        TextView name = rowView.findViewById(R.id.task_name_textView);
+        TextView status = rowView.findViewById(R.id.task_status_textView);
+
+        ID.setText(IDs[position]);
+        name.setText(names[position]);
+        status.setText(statuses[position]);
+
+        return rowView;
+    }
 
 }
