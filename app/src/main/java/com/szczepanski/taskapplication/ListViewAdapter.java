0c95b1e6c5b8cded7b94a193fc70440816883066
Łukasz Szczepański <l.szczepan@wp.pl>
2018-11-13 14:21:37 +0100

    pierwszy commit - przygotowanie głównego widoku

---
+++ app/src/main/java/com/szczepanski/taskapplication/ListViewAdapter.java
@@ -0,0 +1,10 @@
+package com.szczepanski.taskapplication;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+
+public class ListViewAdapter extends ArrayAdapter {
+
+    private Activity context;
+
+}
