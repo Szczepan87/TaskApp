d24f520cab4a512720cbfc74200e6461b4e26bab
Łukasz Szczepański <l.szczepan@wp.pl>
2018-11-15 09:44:33 +0100

    przygotowanie widoku i danych

--- app/src/main/java/com/szczepanski/taskapplication/MainActivity.java
+++ app/src/main/java/com/szczepanski/taskapplication/MainActivity.java
@@ -5,18 +5,22 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView idTextView;
     private TextView nameTextView;
     private TextView statusTextView;
     private Button statusButton;
+    private List<Task> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prepViews();
+        prepData();
 
     }
 
@@ -25,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
         nameTextView = findViewById(R.id.name_textView);
         statusTextView = findViewById(R.id.status_textView);
         statusButton = findViewById(R.id.status_button);
+    }
+
+    private void prepData(){
+        int[] IDs = new int[]{1,2,3,4,5};
+        String[] names = new String[]{"Name 1","Name 2","Name 3","Name 4","Name 5"};
+        String[] stats = new String[]{"Status 1","Status 2","Status 3","Status 4","Status 5"};
+
+        for (int i = 0; i < IDs.length; i++) {
+            taskList.add(new Task(IDs[i],names[i],stats[i]));
+        }
     }
 }
