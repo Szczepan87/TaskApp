9842eb2a7a3bf565c1f15855b09b53ab2c66f912
Łukasz Szczepański <l.szczepan@wp.pl>
2018-11-18 14:26:36 +0100

    test klasy Task

--- app/src/main/java/com/szczepanski/taskapplication/Task.java
+++ app/src/main/java/com/szczepanski/taskapplication/Task.java
@@ -1,5 +1,7 @@
 package com.szczepanski.taskapplication;
 
+import java.util.Objects;
+
 public class Task {
 
     private int id;
@@ -26,5 +28,21 @@ public class Task {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id &&
+                Objects.equals(name, task.name) &&
+                Objects.equals(status, task.status);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, status);
     }
 }
--- app/src/test/java/com/szczepanski/taskapplication/TaskTest.java
+++ app/src/test/java/com/szczepanski/taskapplication/TaskTest.java
@@ -6,19 +6,30 @@ import static org.junit.Assert.*;
 
 public class TaskTest {
 
+    private Task task = new Task(1,"Name", "Status");
+
     @Test
     public void getIdTest() {
+        assertEquals(1,task.getId());
     }
 
     @Test
     public void getNameTest() {
+        assertEquals("Name" ,task.getName());
     }
 
     @Test
     public void getStatusTest() {
+        assertEquals("Status" ,task.getStatus());
     }
 
     @Test
     public void setStatusTest() {
+        task.setStatus("Status 2");
+        assertEquals("Status 2",task.getStatus());
+    }
+
+    @Test
+    public void equalsTest() {
     }
 }
